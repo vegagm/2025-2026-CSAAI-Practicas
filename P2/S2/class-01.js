612aa364b72c5c330233d83154443080b391bfd1
Vega GOnzalez <v.gonzalezm.2023@alumnos.urjc.es>
2026-03-13 20:40:52 +0100

    creamos los archivos para la practica2

---
+++ P2/S2/class-01.js
@@ -0,0 +1,12 @@
+
+const btn1 = document.getElementById('btn1');
+const dsp1 = document.getElementById('dsp1');
+
+btn1.onclick = () => {
+    console.log(msg);
+
+    dsp1.i
+
+    msg = "AAAAAAAAAA zingoña !!!";
+    console.log(msg);
+}

80b39a7e1a9a85933056c5d5c7500d731481543e
Vega GOnzalez <v.gonzalezm.2023@alumnos.urjc.es>
2026-03-21 14:26:22 +0100

    hacmeos el cronometro parte 1

--- P2/crono.js
+++ P2/crono.js
@@ -1,1 +1,39 @@
-// Para gestionar el cronometro
+// Para gestionar el tiempo
+
+let tiempo = 0;
+let intervalo = null;
+
+
+// formateamos el tiempo a 00:00:00
+function formatearTiempo(segundos){
+    let h = Math.floor(segundos / 3600);
+    let m = Math.floor((segundos % 3600) / 60);
+    let s = segundos % 60;
+
+    return `${h}:${m.toString().padStart(2,'0')}:${s.toString().padStart(2,'0')}`;
+
+}
+
+// iniciamos el cronómetro
+function iniciarCrono(){
+
+    if(intervalo !== null) return;  //evitamos duplicados
+
+    intervalo = setInterval( () => {
+        tiempo++;
+        document.getElementById("tiempo").textContent = formatearTiempo(tiempo);
+    }, 1000);
+}
+
+// parar cronómetro
+function pararCrono(){
+    clearInterval(intervalo);
+    intervalo = null;
+}
+
+//resetear cronómetro
+function resetCrono(){
+    pararCrono();
+    tiempo = 0;
+    document.getElementById("tiempo").textContent = "00:00:00";
+}
